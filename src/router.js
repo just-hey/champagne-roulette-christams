b8f0379ca6847fe44cfad75accf89f5f8d46e29b
Justin <justhays124@gmail.com>
2019-04-02 21:58:25 -0600

    updated for background color and height enforcment and added some spacing

--- src/router.js
+++ src/router.js
@@ -22,9 +22,9 @@ function TheRouter() {
 
         <hr />
 
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
+        <Route exact path="/" component={ Home } />
+        <Route path="/about" component={ About } />
+        <Route path="/contact" component={ Contact } />
       </div>
     </Router>
   );
