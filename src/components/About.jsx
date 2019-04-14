1b4334f4efe6dc78cef5a9012c5a9aa6bdf7d43f
cassandraGoose <cassandra.torske@gmail.com>
2019-04-13 19:53:50 -0600

    messy and temporary layout of about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,14 +9,13 @@ class About extends Component {
 
   render() {
     return (
-      <div>
-        <h1>hey it's about</h1>
-        <p>
-          Creating beautiful machined goods is my passion.
-          I am currently the sole designer at <a href="#">SevenK Wheels</a>, a company that manufactures one-off forged wheels for high end custom cars.
-          Also, while acting as the Chief of Design at <a href="#">Tread Global</a> I gained experience designing plastic packaging. During my time there I designed multiple child resistant packaging products for the Medicals Marijana Industry.
-          During my free time I like to design <a href="#">interior lighting</a> solutions.
-        </p>
+      <div className="about">
+        <div className="container">
+          <p>Creating beautiful machined goods is my passion.</p>
+          <p>I am currently the sole designer at <a href="#">SevenK Wheels</a>, a company that manufactures one-off forged wheels for high end custom cars.</p>
+          <p>Also, while acting as the Chief of Design at <a href="#">Tread Global</a> I gained experience designing plastic packaging. During my time there I designed multiple child resistant packaging products for the Medicals Marijana Industry.</p>
+          <p>During my free time I like to design <a href="#">interior lighting</a> solutions.</p>
+        </div>
       </div>
     );
   }
