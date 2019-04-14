ae87fa4dc6b10f558dbbe259a06fc9c79655d288
cassandraGoose <cassandra.torske@gmail.com>
2019-04-13 19:41:52 -0600

    layout and temp font for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,25 +7,17 @@ class Home extends Component {
   };
 
   render() {
-    const className = [
-      'home'
-    ];
-
     return (
-      <div>
-        <h1>hey it's home</h1>
-        <div className={className.join(" ").trim()}>
-          <button onClick={this.handleClick}>{this.props.aProp}</button>
-        </div>
+      <div className="container">
         <p>
           "Once you stop learning and evolving, you're just an asshole."
         </p>
         <hr />
-        <p>
-          This is the portfolio of <a href="#" href="#">Juno Cooper</a>, a creative human who is not an asshole. Constant evolution an expansion of knowledge and abilities is the cornerstone of what makes me a designer.
-          I specialize in <a href="#">Industrial Design</a> & enjoy <a href="#">Graphic Design</a> when the opportunity arises.
-          Currently I live in Denver, Colorado.
-        </p>
+        <div>
+          <p>This is the portfolio of <a href="#" href="#">Juno Cooper</a>, a creative human who is not an asshole. Constant evolution an expansion of knowledge and abilities is the cornerstone of what makes me a designer.</p>
+          <p>I specialize in <a href="#">Industrial Design</a> & enjoy <a href="#">Graphic Design</a> when the opportunity arises.</p>
+          <p>Currently I live in Denver, Colorado.</p>
+        </div>
       </div>
     );
   }
