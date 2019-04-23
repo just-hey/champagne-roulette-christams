d96d1e4d8b443de5d2aaaac45a1c0b1d603c0972
Justin <justhays124@gmail.com>
2019-04-22 19:04:55 -0600

    router revamp. no there are not vampires in the code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
-import TheRouter from './router.jsx';
+import { BrowserRouter as Router, Redirect, Route, Switch, withRouter } from 'react-router-dom';
 import './styles/App.css';
 import AboutComponent from './components/About.jsx';
 import About2Component from './components/About2.jsx';
 import AdminLogin from './components/AdminLogin.jsx';
 import ContactComponent from './components/Contact.jsx';
+import Home from './components/Home.jsx'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <TheRouter/>
-        </header>
-        <AboutComponent/>
-        <About2Component/>
-        <ContactComponent/>
-        <AdminLogin />
-      </div>
+    <Router className='App'>
+      <React.Fragment>
+        <Switch>
+          <Route exact path='/' component={ () => (
+            <div> 
+              <Home />
+              <AboutComponent />
+              <About2Component />
+              <ContactComponent />
+              <AdminLogin />
+            </div> 
+          )} />
+          <Route exact path='/home' component={ Home } /> 
+          <Route exact path='/about' component={ AboutComponent } />
+          <Route exact path='/about2' component={ About2Component } />
+          <Route exact path='/ContactComponent' component={ ContactComponent } />
+          <Route exact path='/admin' component={ AdminLogin  } />
+        </Switch>
+      </React.Fragment>
+    </Router>
     );
   }
 }
